7fe275f49f5e8c23bd54bd622a4b37161be99899
Ansof Habibunnadjar <ansofhn5@gmail.com>
2023-01-04 14:11:42 +0700

    feat: add head for cms

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <div className="bg-softWhite">
+      <Head>
+        <title>Encrease - CMS</title>
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </div>
