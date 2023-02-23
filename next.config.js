4763033771f8555900db56a433a10f5e269cad1d
Ansof Habibunnadjar <ansofhn5@gmail.com>
2023-02-23 19:49:31 +0700

    fix: ignore eslint when build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [{ protocol: "http", hostname: "49.0.2.250" }],
   },
